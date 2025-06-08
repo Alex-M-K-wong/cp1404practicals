cd91302d1db87864da598f9360749eb8becd0685
Alex-M-K-wong <mankit.wong@my.jcu.edu.au>
2025-06-08 16:26:09 +1000

    Add a finished randoms.py

---
+++ prac_03/randoms.py
@@ -0,0 +1,20 @@
+#What did you see on line 1?
+#17
+#What was the smallest number you could have seen, what was the largest?
+#smallest: 5, largest: 20
+
+#What did you see on line 2?
+#3
+#What was the smallest number you could have seen, what was the largest?
+#smallest: 3, largest: 9
+#Could line 2 have produced a 4?
+#No, because step is 2 starting from 3
+
+#What did you see on line 3?
+#3.41968848111704
+#What was the smallest number you could have seen, what was the largest?
+#Smallest: 2.5, Largest: 5.5
+
+#Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+import random
+print(random.randint(0,100))
