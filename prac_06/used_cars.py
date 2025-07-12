3a52e815dbbf35ae479eb509e05c8edb04326d0a
Alex-M-K-wong <mankit.wong@my.jcu.edu.au>
2025-07-12 16:54:05 +1000

    finished car.py and used_cars.py

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -13,6 +13,13 @@ def main():
     my_car.drive(30)
     print(f"Car has fuel: {my_car.fuel}")
     print(my_car)
+    limo = Car(fuel=100, name="mazda cx-5")
+    limo.add_fuel(amount=20)
+    print(f"Car has fuel: {limo.fuel}")
+    limo.drive(distance=115)
+    print(f"Car has fuel: {limo.fuel}")
+    print(limo)
+
 
 
 main()
