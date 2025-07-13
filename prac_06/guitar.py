c8fe685c55dae5ce6a45ed903ee56122d1a6cfb7
Alex-M-K-wong <mankit.wong@my.jcu.edu.au>
2025-07-13 15:48:31 +1000

    finished guitar.py

---
+++ prac_06/guitar.py
@@ -0,0 +1,23 @@
+CORRECT_YEAR = 2025
+AGE_LIMIT =50
+
+class Guitar:
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+      return f"{self.name} ({self.year} : ${self.cost})"
+
+    def get_age(self):
+        age = CORRECT_YEAR - self.year
+        return age
+
+
+    def is_vintage(self):
+        age = self.get_age()
+        if age >= AGE_LIMIT:
+            return True
+        else:
+            return False
